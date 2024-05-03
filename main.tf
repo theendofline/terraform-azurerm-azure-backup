2ec0341e6e4ec53e9653dc82bbcf8b81072ce75c
theendofline <19670612+theendofline@users.noreply.github.com>
2024-05-04 01:08:30 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 #---------------------------------
 # Local declarations
 #---------------------------------
-locals { 
+locals {
   name                = var.name == "" ? "-backup" : "-${var.name}"
   resource_group_name = element(coalescelist(data.azurerm_resource_group.rgrp.*.name, azurerm_resource_group.rg.*.name, [""]), 0)
   resource_prefix     = var.resource_prefix == "" ? local.resource_group_name : var.resource_prefix
   location            = element(coalescelist(data.azurerm_resource_group.rgrp.*.location, azurerm_resource_group.rg.*.location, [""]), 0)
 
-  virtual_machines = { 
+  virtual_machines = {
     for idx, vm in var.backup_virtual_machines : vm.name => {
-       idx : idx,
-       vm : vm,
+      idx : idx,
+      vm : vm,
     }
   }
 
@@ -21,9 +21,10 @@ locals {
     }
   }
 
-  timeout_create  = "180m"
-  timeout_delete  = "60m"
-  timeout_read    = "60m"
+  timeout_create = "180m"
+  timeout_delete = "60m"
+  timeout_read   = "60m"
+  timeout_update = "60m"
 }
 
 #---------------------------------------------------------
@@ -39,12 +40,13 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 
-  tags     = merge({ "ResourceName" = "${var.resource_group_name}" }, var.tags, )
+  tags = merge({ "ResourceName" = "${var.resource_group_name}" }, var.tags, )
 
   timeouts {
-    create  = local.timeout_create
-    delete  = local.timeout_delete
-    read    = local.timeout_read
+    create = local.timeout_create
+    delete = local.timeout_delete
+    read   = local.timeout_read
+    update = local.timeout_update
   }
 }
 
@@ -58,16 +60,17 @@ resource "azurerm_recovery_services_vault" "vault" {
   resource_group_name = local.resource_group_name
   sku                 = var.recovery_services_vault_sku != null ? var.recovery_services_vault_sku : "Standard"
   storage_mode_type   = var.recovery_services_vault_storage_mode != null ? var.recovery_services_vault_storage_mode : "LocallyRedundant"
-  
+
   #cross_region_restore_enabled = var.recovery_servuces_vault_cross_region_restore_enabled
 
-  tags     = merge({ "ResourceName" = var.recovery_services_vault_name != "" ? var.recovery_services_vault_name : "${local.resource_prefix}-bvault" }, var.tags, )
+  tags = merge({ "ResourceName" = var.recovery_services_vault_name != "" ? var.recovery_services_vault_name : "${local.resource_prefix}-bvault" }, var.tags, )
 
   timeouts {
-    create  = local.timeout_create
-    delete  = local.timeout_delete
-    read    = local.timeout_read
-  }  
+    create = local.timeout_create
+    delete = local.timeout_delete
+    read   = local.timeout_read
+    update = local.timeout_update
+  }
 }
 
 #-------------------------------------
@@ -99,8 +102,8 @@ resource "azurerm_backup_policy_vm" "policy" {
     for_each = var.backup_polcy_retention_weekly_count != "" ? [1] : []
 
     content {
-      count = var.backup_polcy_retention_weekly_count
-      weekdays = var.backup_policy_retention_weekly_weekdays != null ? var.backup_policy_retention_weekly_weekdays : [ "Saturday" ]
+      count    = var.backup_polcy_retention_weekly_count
+      weekdays = var.backup_policy_retention_weekly_weekdays != null ? var.backup_policy_retention_weekly_weekdays : ["Saturday"]
     }
   }
 
@@ -108,16 +111,17 @@ resource "azurerm_backup_policy_vm" "policy" {
     for_each = var.backup_polcy_retention_monthly_count != "" ? [1] : []
 
     content {
-      count = var.backup_polcy_retention_monthly_count
-      weekdays  = var.backup_policy_retention_monthly_weekdays != null ? var.backup_policy_retention_monthly_weekdays : [ "Saturday" ]
-      weeks     = [ "Last" ]
+      count    = var.backup_polcy_retention_monthly_count
+      weekdays = var.backup_policy_retention_monthly_weekdays != null ? var.backup_policy_retention_monthly_weekdays : ["Saturday"]
+      weeks    = ["Last"]
     }
-  }  
+  }
 
   timeouts {
-    create  = local.timeout_create
-    delete  = local.timeout_delete
-    read    = local.timeout_read
+    create = local.timeout_create
+    delete = local.timeout_delete
+    read   = local.timeout_read
+    update = local.timeout_update
   }
 }
 
@@ -145,8 +149,8 @@ resource "azurerm_backup_policy_file_share" "policy" {
     for_each = var.backup_polcy_retention_weekly_count != "" ? [1] : []
 
     content {
-      count = var.backup_polcy_retention_weekly_count
-      weekdays = var.backup_policy_retention_weekly_weekdays != null ? var.backup_policy_retention_weekly_weekdays : [ "Saturday" ]
+      count    = var.backup_polcy_retention_weekly_count
+      weekdays = var.backup_policy_retention_weekly_weekdays != null ? var.backup_policy_retention_weekly_weekdays : ["Saturday"]
     }
   }
 
@@ -154,16 +158,17 @@ resource "azurerm_backup_policy_file_share" "policy" {
     for_each = var.backup_polcy_retention_monthly_count != "" ? [1] : []
 
     content {
-      count = var.backup_polcy_retention_monthly_count
-      weekdays  = var.backup_policy_retention_monthly_weekdays != null ? var.backup_policy_retention_monthly_weekdays : [ "Saturday" ]
-      weeks     = [ "Last" ]
+      count    = var.backup_polcy_retention_monthly_count
+      weekdays = var.backup_policy_retention_monthly_weekdays != null ? var.backup_policy_retention_monthly_weekdays : ["Saturday"]
+      weeks    = ["Last"]
     }
-  }  
+  }
 
   timeouts {
-    create  = local.timeout_create
-    delete  = local.timeout_delete
-    read    = local.timeout_read
+    create = local.timeout_create
+    delete = local.timeout_delete
+    read   = local.timeout_read
+    update = local.timeout_update
   }
 }
 
@@ -172,7 +177,7 @@ resource "azurerm_backup_policy_file_share" "policy" {
 #-------------------------------------
 
 data "azurerm_virtual_machine" "vm" {
-  for_each            = local.virtual_machines
+  for_each = local.virtual_machines
 
   name                = each.value.vm.name
   resource_group_name = each.value.vm.resource_group_name != "" ? each.value.vm.resource_group_name : local.resource_group_name
@@ -189,22 +194,23 @@ data "azurerm_virtual_machine" "vm" {
 # }
 
 resource "azurerm_backup_protected_vm" "vm" {
-  for_each            = local.virtual_machines
+  for_each = local.virtual_machines
 
   resource_group_name = local.resource_group_name
   recovery_vault_name = azurerm_recovery_services_vault.vault.name
   backup_policy_id    = azurerm_backup_policy_vm.policy.id
 
-  source_vm_id        = data.azurerm_virtual_machine.vm[each.value.vm.name].id
+  source_vm_id = data.azurerm_virtual_machine.vm[each.value.vm.name].id
 
   # depends_on = [
   #   azurerm_virtual_machine_extension.vm_extensions
   # ]
 
   timeouts {
-    create  = local.timeout_create
-    delete  = local.timeout_delete
-    read    = local.timeout_read
+    create = local.timeout_create
+    delete = local.timeout_delete
+    read   = local.timeout_read
+    update = local.timeout_update
   }
 }
 
@@ -213,33 +219,34 @@ resource "azurerm_backup_protected_vm" "vm" {
 #-------------------------------------
 
 data "azurerm_storage_account" "storage_backup" {
-  for_each            = local.file_shares
+  for_each = local.file_shares
 
   name                = each.value.file_share.storage_account_name
   resource_group_name = each.value.file_share.resource_group_name
 }
 
 resource "azurerm_backup_container_storage_account" "container" {
-  for_each            = local.file_shares
+  for_each = local.file_shares
 
   recovery_vault_name = azurerm_recovery_services_vault.vault.name
   resource_group_name = local.resource_group_name
 
-  storage_account_id  = data.azurerm_storage_account.storage_backup[each.value.file_share.name].id
+  storage_account_id = data.azurerm_storage_account.storage_backup[each.value.file_share.name].id
 
   depends_on = [
     data.azurerm_storage_account.storage_backup
   ]
 
   timeouts {
-    create  = local.timeout_create
-    delete  = local.timeout_delete
-    read    = local.timeout_read
-  }  
+    create = local.timeout_create
+    delete = local.timeout_delete
+    read   = local.timeout_read
+    update = local.timeout_update
+  }
 }
 
 resource "azurerm_backup_protected_file_share" "share" {
-  for_each            = local.file_shares
+  for_each = local.file_shares
 
   resource_group_name = local.resource_group_name
   recovery_vault_name = azurerm_recovery_services_vault.vault.name
@@ -253,9 +260,9 @@ resource "azurerm_backup_protected_file_share" "share" {
   ]
 
   timeouts {
-    create  = local.timeout_create
-    delete  = local.timeout_delete
-    read    = local.timeout_read
-    update  = local.timeout_update
+    create = local.timeout_create
+    delete = local.timeout_delete
+    read   = local.timeout_read
+    update = local.timeout_update
   }
 }
